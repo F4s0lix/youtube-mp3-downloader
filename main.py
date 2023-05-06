e8831e64093afc61f0deed3d55cc3974e29f628d
F4s0lix <f4s0lix@protonmail.com>
2023-05-06 12:50:33 +0200

    change technology

--- main.py
+++ main.py
@@ -1,63 +1,12 @@
-from pytube import YouTube
-import os
-from pytube import Playlist
+from __future__ import unicode_literals
+import youtube_dl
+import sys
 
+link = sys.argv[1]
 
-def playlist_downloader(link):
-    """
-    Downloads a playlist from YouTube or YouTube Music
-    :param link: link of the playlist
-    :return: None, only downloads the playlist in ./music directory and printing confirmation message
-    """
-    playlist = Playlist(link)
-
-    for video in playlist.videos:
-        stream = video.streams.filter(only_audio=True).first()
-
-
-        out_file = stream.download('./music')
-
-
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
-        os.rename(out_file, new_file)
-
-
-        print(stream.title + " has been downloaded successfully")
-
-
-def song_downloader(link):
-    """
-    Downloads a single song from YouTube or YouTube Music
-    :param link: link of the song
-    :return: None, only downloads the song in ./music directory and printing confirmation message
-    """
-    video = link.streams.filter(only_audio=True).first()
-    
-    
-    out_file = video.download("./music")
-    
-    
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    
-    
-    print(link.title + " has been downloaded successfully.")
-
-
-
-
-
-
-if __name__ == "__main__":
-    """
-    Checks if user wants to download a playlist or a single song and evokes right function
-    """
-    check = str(input("Do you want to download a playlist or one song? (p/o) "))
-    if check == "p":
-        playlist_downloader(str(input("Enter the playlist link: ")))
-    elif check == "o":
-        song_downloader(str(input("Enter the song link: ")))
-    else:
-        print("Invalid input")
+ydl_options = {'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3','preferredquality': '192'}]}
+with youtube_dl.YoutubeDL(ydl_options) as ydl:
+    try:
+        ydl.download([link])
+    except:
+        print("error. Try again")
